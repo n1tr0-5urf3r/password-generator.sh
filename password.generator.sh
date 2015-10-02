3df8f7c5a71704bcbabe1dffeeb1328751763b42
n1tr0-5urf3r <Fabian_Ihle@t-online.de>
2015-10-02 08:51:01 +0200

    Initial commit
    Simple password generator written in bash

---
+++ password.generator.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# PASSWORDGENERATOR
+
+# Parameter
+# $1 password length
+
+printUsage(){
+        echo -e "\nUsage: "
+       echo "./password-generator.sh length(int)"
+        echo -e "\n"
+}
+
+# Test if all parameter available
+if [ "$#" -ne 1 ]
+        then
+                printUsage
+                echo "Missing or too many arguments"
+                exit 1
+fi
+
+chars=(a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0 )
+availableChars=${#chars[@]}
+
+for i in $(seq 1 $1); do
+        nextChar=$(($RANDOM % $availableChars))
+        echo ${chars[$nextChar]} >> /usr/local/bin/password.temp
+done
+
+echo -e "\n"
+cat /usr/local/bin/password.temp | perl -pe 's/\n//'
+echo -e "\n"
+rm /usr/local/bin/password.temp
